0011b906984d053bf65e90c2215f1586f59b3438
JoanChirinos <jchirinos@stuy.edu>
2018-09-27 21:58:46 -0400

    better strlen + implemented strcpy

--- string.c
+++ string.c
@@ -1,16 +1,27 @@
 #include <stdio.h>
 #include <string.h>
 
-int my_strlen(char *s) {
-  int ctr = 0;
-  while(*s) {
-    s++;
-    ctr++;
-  }
-  return ctr;
+int my_strlen(char *str) {
+  char* s; //inst char pointer
+  for(s = str; *s; s++); //traverses through str until null
+  return (s - str); //subtracts memory addresses
+}
+
+char * my_strcpy(char *dst, char *src) {
+  char* original_location = dst;
+  while ((*dst++ = *src++));
+  return original_location;
 }
 
 int main() {
   char *s = "Hello";
-  printf("TESTING STRLEN\nLength of %s:\n\tstrlen: %d\n\tmy_strlen: %d\n", s, strlen(s), my_strlen(s));
+  printf("TESTING STRLEN\nLength of %s:\n\tstrlen: %lu\n\tmy_strlen: %d\n\n", s, strlen(s), my_strlen(s));
+
+  char dst[256] = "hh";
+  char src[] = "Copy this";
+
+
+  char my_dst[256] = "hh";
+  char my_src[] = "Copy this";
+  printf("TESTING STRCPY\nTo copy: %s\nstrcopy(dst, src) --> %s\nmy_strcpy(dst, src) --> %s\n", src, strcpy(dst, src), my_strcpy(my_dst, my_src));
 }
